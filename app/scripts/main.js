fd3c818e71ffa2f10851c435710ef95b22fefbe8
ferengere <ferengere@gmail.com>
2015-05-21 16:57:53 +0200

    primer build

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,8 @@
 'use strict';
 $(document).ready(function() {
     console.log('LET\'S GET BUSY!');
+    var $bc = $('ol.breadcrumb');
+    $bc.on('click', 'a', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
 });
