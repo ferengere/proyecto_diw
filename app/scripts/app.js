aa73a9692a38a0de3198976ae7c84d8f3ead7bb9
ferengere <ferengere@gmail.com>
2015-05-21 20:07:52 +0200

    rutas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,56 @@
 
 /**
  * @ngdoc overview
- * @name proyectoDiwApp
+ * @name testAngularApp
  * @description
- * # proyectoDiwApp
+ * # testAngularApp
  *
  * Main module of the application.
  */
 angular
-  .module('proyectoDiwApp', []);
+    .module('webApp', [
+        /*'ngAnimate',*/
+        /*'ngAria',*/
+        /*'ngCookies',*/
+        /*'ngMessages',*/
+        /*'ngResource',*/
+        'ngRoute',
+        /*'ngSanitize',*/
+        /*'ngTouch'*/
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .
+        when('/inicio', {
+            templateUrl: 'views/inicio.html'
+                /*,
+                                  controller: 'InicioCtrl as inicio'*/
+        }).
+        when('/carta', {
+            templateUrl: 'views/carta.html',
+            controller: 'CartaCtrl as carta'
+        }).
+        when('/menus', {
+            templateUrl: 'views/menus.html'
+                /*,
+                                  controller: 'MenusCtrl as menus'*/
+        }).
+        when('/galeria', {
+            templateUrl: 'views/galeria.html'
+                /*,
+                                  controller: 'GaleriaCtrl as galeria'*/
+        }).
+        when('/reservas', {
+            templateUrl: 'views/reservas.html'
+                /*,
+                                  controller: 'ReservasCtrl as reservas'*/
+        }).
+        when('/contacto', {
+            templateUrl: 'views/contacto.html'
+                /*,
+                                  controller: 'ContactoCtrl as contacto'*/
+        }).
+        otherwise({
+            redirectTo: '/inicio'
+        });
+    });
