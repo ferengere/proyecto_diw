6e5f427fca46997f49e89fd2c81b248b30cfc89a
ferengere <ferengere@gmail.com>
2015-05-26 19:41:13 +0200

    sticky y cabecera

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,57 +1,11 @@
 'use strict';
+var mainAppModuleName = 'webApp';
+var mainAppModule = angular.module(mainAppModuleName, ['ngRoute']);
+mainAppModule.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}]);
 
-/**
- * @ngdoc overview
- * @name testAngularApp
- * @description
- * # testAngularApp
- *
- * Main module of the application.
- */
-angular
-    .module('webApp', [
-        /*'ngAnimate',*/
-        /*'ngAria',*/
-        /*'ngCookies',*/
-        /*'ngMessages',*/
-        /*'ngResource',*/
-        'ngRoute',
-        /*'ngSanitize',*/
-        /*'ngTouch'*/
-    ])
-    .config(function($routeProvider) {
-        $routeProvider
-            .
-        when('/inicio', {
-            templateUrl: 'views/inicio.html'
-                /*,
-                                  controller: 'InicioCtrl as inicio'*/
-        }).
-        when('/carta', {
-            templateUrl: 'views/carta.html',
-            controller: 'CartaCtrl as carta'
-        }).
-        when('/menus', {
-            templateUrl: 'views/menus.html'
-                /*,
-                                  controller: 'MenusCtrl as menus'*/
-        }).
-        when('/galeria', {
-            templateUrl: 'views/galeria.html'
-                /*,
-                                  controller: 'GaleriaCtrl as galeria'*/
-        }).
-        when('/reservas', {
-            templateUrl: 'views/reservas.html'
-                /*,
-                                  controller: 'ReservasCtrl as reservas'*/
-        }).
-        when('/contacto', {
-            templateUrl: 'views/contacto.html'
-                /*,
-                                  controller: 'ContactoCtrl as contacto'*/
-        }).
-        otherwise({
-            redirectTo: '/inicio'
-        });
-    });
+angular.element(document).ready(function() {
+    //angular.bootstrap(document, [mainAppModuleName]);
+    console.log('READY');
+});
